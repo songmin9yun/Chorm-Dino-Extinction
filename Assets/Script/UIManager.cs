5066f1dba66d6abd24e7b54d7c8f31d9cbc5d592
songmin9yun <songmin9yun@gmail.com>
2026-05-16 20:20:01 +0900

    add heal

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI hpText;
+ 
+    
+    void Start()
+    {
+        if(scoreText != null)
+        {
+            scoreText.text = $"Score: {Poop.Score}";
+        }
+
+        if (hpText != null)
+        {
+            hpText.text = $"HP: {PlayerMove.hp}";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(scoreText != null)
+        {
+            scoreText.text = $"Score: {Poop.Score}";
+        }
+
+        if (hpText != null)
+        {
+            hpText.text = $"HP: {PlayerMove.hp}";
+        }
+    }
+    
+}
